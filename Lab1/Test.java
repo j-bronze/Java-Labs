622adb8f23dc1f0041aced4c49365cf89f0d9cd4
j-bronze <boroznovskaya@tut.by>
2018-03-16 16:50:39 +0300

    first commit

---
+++ Lab1/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+	public static void main(String[] args){
+		System.out.println("\t Name: \t\t\t" + "Boroznovskaya E.S.");
+		System.out.println("\t Group: \t\t" + "pois 1706v");
+		System.out.println("\t Primary activity: \t" + "work, study");
+		System.out.println("\t Interests: \t\t" + "dance, cycling, tresses, reading");
+	}
+}
