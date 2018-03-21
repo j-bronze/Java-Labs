b1d0f4fa2e8dabbcb52827a84ee76514a06b311a
j-bronze <boroznovskaya@tut.by>
2018-03-21 16:43:03 +0300

    commit lab2

---
+++ Lab2/Questionnaire.java
@@ -0,0 +1,9 @@
+public class Questionnaire{
+	public static void main(String[] args){
+		System.out.println("Укажите место работы:");
+
+		System.out.println("Укажите свою должность:");
+
+		System.out.println("Укажите продолжительность рабочего дня:");
+	}
+}
