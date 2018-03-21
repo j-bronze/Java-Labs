437c5298be585f2f45e87e8e0fdcf331bb91b429
j-bronze <boroznovskaya@tut.by>
2018-03-21 18:17:54 +0300

    commit task1

--- Lab2/Questionnaire.java
+++ Lab2/Questionnaire.java
@@ -1,9 +1,19 @@
 public class Questionnaire{
 	public static void main(String[] args){
 		System.out.println("Укажите место работы:");
-
+		Scanner scn1 = new Scanner(System.in);
+		String value1 = scn1.nextLine();
+		
 		System.out.println("Укажите свою должность:");
-
+		Scanner scn2 = new Scanner(System.in);
+		String value2 = scn2.nextLine();
+		
 		System.out.println("Укажите продолжительность рабочего дня:");
+		Scanner scn3 = new Scanner(System.in);
+		int value3 = scn3.nextInt();
+		
+		System.out.printf("Место работы: %20s\n", value1);
+		System.out.printf("Должность: %20s\n", value2);
+		System.out.printf("Продолжительность рабочего дня: %10s\n", value3);
 	}
 }
