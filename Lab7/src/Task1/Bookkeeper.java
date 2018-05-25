ee4dd445da75258c1c13b887973ab1305b2d5b75
j-bronze <boroznovskaya@tut.by>
2018-05-25 23:43:52 +0300

    update logic

--- Lab7/src/Task1/Bookkeeper.java
+++ Lab7/src/Task1/Bookkeeper.java
@@ -1,27 +1,46 @@
 package Task1;
 
 public class Bookkeeper {
-	public static int countQuantityOfGoods(Store stockroom) {
-		int quantity = stockroom.getGood1().getQuantity() + 
-					stockroom.getGood2().getQuantity() + 
-					stockroom.getGood3().getQuantity();
+	int quantity = 0;
+		if (stockroom.getGood1().isAvailability()) {
+			quantity += stockroom.getGood1().getQuantity();
+		}
+		if (stockroom.getGood2().isAvailability()) {
+			quantity += stockroom.getGood2().getQuantity();
+		}
+		if (stockroom.getGood3().isAvailability()) {
+			quantity += stockroom.getGood3().getQuantity();
+		}
 		
 		return quantity;
 	}
 	
 	public static double countCommonPrice(Store stockroom) {
-		
-		double sum = stockroom.getGood1().getPrice()*stockroom.getGood1().getQuantity() + 
-				stockroom.getGood2().getPrice()*stockroom.getGood2().getQuantity() +
-				stockroom.getGood3().getPrice()*stockroom.getGood3().getQuantity();
+		double sum = 0;
+		if (stockroom.getGood1().isAvailability()) {
+			sum += stockroom.getGood1().getPrice() * stockroom.getGood1().getQuantity();
+		}
+		if (stockroom.getGood2().isAvailability()) {
+			sum += stockroom.getGood2().getPrice() * stockroom.getGood2().getQuantity();
+		}
+		if (stockroom.getGood3().isAvailability()) {
+			sum += stockroom.getGood3().getPrice() * stockroom.getGood3().getQuantity();
+		}
 		
 		return sum;
 	}
 	
 	public static int countTotalWeight(Store stockroom) {
-		int totalWeight = stockroom.getGood1().getWeight()*stockroom.getGood1().getQuantity() +
-				stockroom.getGood2().getWeight()*stockroom.getGood2().getQuantity() +
-				stockroom.getGood3().getWeight()*stockroom.getGood3().getQuantity();
+		int totalWeight = 0;
+		if (stockroom.getGood1().isAvailability()) {
+			totalWeight += stockroom.getGood1().getWeight() * stockroom.getGood1().getQuantity();
+		}
+		if (stockroom.getGood2().isAvailability()) {
+			totalWeight += stockroom.getGood2().getWeight() * stockroom.getGood2().getQuantity();
+		}
+		if (stockroom.getGood3().isAvailability()) {
+			totalWeight += stockroom.getGood3().getWeight() * stockroom.getGood3().getQuantity();
+		}
 		
 		return totalWeight;
 	}
